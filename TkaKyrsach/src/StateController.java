b6fe984b9977d7461d66c44f27066a49377c639b
Enego-dev <themrxxxyou@gmail.com>
2025-12-01 01:10:50 +0300

    капец

--- TkaKyrsach/src/StateController.java
+++ TkaKyrsach/src/StateController.java
@@ -4,7 +4,7 @@ import states.TransporterMoveDirection;
 
 public class StateController {
     // region Управляющие сигналы (сделать что-то)
-    private static boolean[] Vi = new boolean[3];  // Открыть задвижку резервуаров
+    private static boolean[] Vi = new boolean[3];  // Открыть задвижку резервуаров -- индексирование с 0 до 2
     private static boolean MR = false; // Транспортер вправо 0-1
     private static boolean ML = false; // Транспортер влево 0-1
     private static boolean MKR = false;    // Повернуть контейнер вправо для сброса в B0-B3 0-1
@@ -92,13 +92,13 @@ public class StateController {
     // endregion
 
     // region Информационные сигналы (проверить что-то)
-    private static boolean[] DRi = new boolean[3]; // Опустошен ли резервуар Ri
-    private static boolean[] DVi = new boolean[3]; // Открыт ли резервуар Ri
-    private static boolean[] DPi = new boolean[4]; // Находится ли контейнер под резервуаром Ri или он в P0
+    private static boolean[] DRi = new boolean[3]; // Опустошен ли резервуар Ri -- индексирование с 0 до 2 -- реальное с 1 до 3
+    private static boolean[] DVi = new boolean[3]; // Открыт ли резервуар Ri -- индексирование с 0 до 2 -- реальное с 1 до 3
+    private static boolean[] DPi = new boolean[4]; // Находится ли контейнер под резервуаром Ri или он в P0 -- индексирование с 0 до 3 -- реальное с 0 до 3
     private static boolean DKL = false;    // Поворачивается ли контейнер влево
     private static boolean DKR = false;    // Поворачивается ли контейнер вправо
     private static boolean DN = true;  // В нейтральном ли положении контейнер
-    private static boolean[] Dj = new boolean[8];  // Заполнен ли бункер Dj
+    private static boolean[] Dj = new boolean[8];  // Заполнен ли бункер Dj -- индексирование с 0 до 7 -- реальное с 0 до 7
     private static boolean BK0 = false;    // Вышел ли контейнер за заданный левый предел
     private static boolean BK1 = false;    // Вышел ли контейнер за заданный правый предел
     private static boolean DT = false; // Сработал ли таймер заполнения
@@ -113,13 +113,18 @@ public class StateController {
 
     // Положение:
     // <0 - аварийная точка BK0
-    // движение контейнера в 0-100, где 10 - P0, 20 - P1, 30 - P2, 40 - P3
+    // движение контейнера в 0-50, где 10 - P0, 20 - P1, 30 - P2, 40 - P3
     // >50 - аварийная точка BK1
     private static short containerPosition = 5;
     // endregion
 
     static void main() {
         while (true){
+            if(shouldStayInIdle()){
+                containerState = ContainerState.IDLE;
+                continue;
+            }
+
             switch (containerState){
                 case IDLE -> {break;}
                 case MOVING_TO_LOAD -> {movingToLoad(); break;}
@@ -149,6 +154,15 @@ public class StateController {
     }
 
     private static void emergencyStop(){
-        
+
     }
+
+    // region Проверки на доступность выполнение хотя бы одной операции
+    public static boolean shouldStayInIdle() {
+        boolean cannotOp1 = Dj[5] || DRi[0] || DRi[2];  // Нельзя выполнить операцию1
+        boolean cannotOp2 = Dj[2] || DRi[0] || DRi[1];  // Нельзя выполнить операцию2
+
+        return cannotOp1 && cannotOp2;  // Нельзя выполнить НИ ОДНУ операцию
+    }
+    // endregion
 }
