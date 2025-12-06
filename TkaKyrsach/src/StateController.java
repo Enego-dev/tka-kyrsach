8a74b0b9a37671bceb67ebd04dacc9b9dea707a7
Enego-dev <themrxxxyou@gmail.com>
2025-12-06 18:55:20 +0300

    Запуск из-под файла TkaKyrsach.bat

--- TkaKyrsach/src/StateController.java
+++ TkaKyrsach/src/StateController.java
@@ -1,8 +1,30 @@
-void main() {
-    // ДЛЯ ПОИСКА ГДЕ МОЖНО СДЕЛАТЬ ПОДЛЯНКУ В ПОИСКЕ ТАК И НАПИСАТЬ "ПОДЛЯНК")))
+import java.util.Scanner;
 
-    // подлянку можно сделать при изменении условий
-    // по дефолту 0b101, 0b101, 0b011, 0b010
-    var automaticMachine = new AutomaticMachineCore(new int[]{0b101, 0b101, 0b011, 0b010});
-    automaticMachine.run();
+public class StateController {
+    static void main() {
+        // ДЛЯ ПОИСКА ГДЕ МОЖНО СДЕЛАТЬ ПОДЛЯНКУ В ПОИСКЕ ТАК И НАПИСАТЬ "ПОДЛЯНК")))
+
+        // подлянку можно сделать при изменении условий
+        // по дефолту 0b101, 0b101, 0b011, 0b010
+
+        IO.println("Введите 4 управляющих кода через пробел! Пример:\n101 101 011 010");
+
+        var scanner = new Scanner(System.in);
+        var line = scanner.nextLine();
+        var rawCodes = line.split(" ");
+        if(line.isEmpty() || rawCodes.length != 4)
+            throw new RuntimeException("Введите 4 управляющих кода через пробел! Пример:\n101 101 011 010");
+
+        var codes = new int[4];
+        for (int i = 0; i < rawCodes.length; i++) {
+            var num = Integer.parseInt(rawCodes[i], 2);
+            if (num > 7)
+                throw new IllegalArgumentException("Число должно состоять из 3-х разрядов!\nИли же быть меньше 8 в 10чной СС!");
+            codes[i] = num;
+        }
+
+        IO.println();
+        var automaticMachine = new AutomaticMachineCore(codes);
+        automaticMachine.run();
+    }
 }
---
+++ Курсач/TkaKyrsach.bat
@@ -0,0 +1,3 @@
+@echo off
+java -jar "C:\Users\themr\IdeaProjects\tka-kyrsach\TkaKyrsach\out\artifacts\TkaKyrsach_jar\TkaKyrsach.jar"
+pause
